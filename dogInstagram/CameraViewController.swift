09756df1f551c94068011fc1ca45360b94da490d
sasakazu <dropout.college2014@gmail.com>
2017-10-21 19:34:29 +0900

    tab bar controller changed.....

---
+++ dogInstagram/CameraViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CameraViewController.swift
+//  dogInstagram
+//
+//  Created by 笹倉 一也 on 2017/10/21.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cameraView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- dogInstagram/ViewController.swift
+++ dogInstagram/ViewController.swift
@@ -12,7 +12,10 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+ 
+    
     @IBOutlet weak var cameraView: UIImageView!
+    
     
     @IBOutlet weak var label: UILabel!
     
@@ -39,6 +42,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                     cameraPicker.delegate = self
                     self.present(cameraPicker, animated: true, completion: nil)
         
+                    
                 }
                 else{
                     label.text = "error"
@@ -73,20 +77,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
+
     
     @IBAction func savePicture(_ sender: Any) {
         let image:UIImage! = cameraView.image
         
-        if image != nil {
-            UIImageWriteToSavedPhotosAlbum(image, self,        #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)),
-            nil)
-        }
-            
-        else{
-            label.text = "image Failed !"
-        }
+                if image != nil {
+                    UIImageWriteToSavedPhotosAlbum(image, self,        #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)),
+                    nil)
+                }
+        
+                else{
+                    label.text = "image Failed !"
+                }
+        
+        
         
     }
+    
+    
+    
+    
     
     // 書き込み完了結果の受け取り
     @objc func image(_ image: UIImage,
@@ -102,25 +113,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+
+    
     @IBAction func showAlbum(_ sender: Any) {
-        
+     
         let sourceType:UIImagePickerControllerSourceType =
-            UIImagePickerControllerSourceType.photoLibrary
+                    UIImagePickerControllerSourceType.photoLibrary
         
         if UIImagePickerController.isSourceTypeAvailable(
             UIImagePickerControllerSourceType.photoLibrary){
             // インスタンスの作成
             let cameraPicker = UIImagePickerController()
-            cameraPicker.sourceType = sourceType
-            cameraPicker.delegate = self
-            self.present(cameraPicker, animated: true, completion: nil)
-            
-            label.text = "Tap the [Start] to save a picture"
-        }
-        else{
-            label.text = "error"
-            
-        }
+                cameraPicker.sourceType = sourceType
+                cameraPicker.delegate = self
+                self.present(cameraPicker, animated: true, completion: nil)
+        
+                    label.text = "Tap the [Start] to save a picture"
+                }
+                else{
+                    label.text = "error"
+        
+                }
     }
     
     
