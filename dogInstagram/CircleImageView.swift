6c13095e80b7cce18c942985f44249b4734d0528
sasakazu <dropout.college2014@gmail.com>
2017-10-31 19:28:12 +0900

    image circle done,,....

---
+++ dogInstagram/CircleImageView.swift
@@ -0,0 +1,22 @@
+//
+//  CircleImageView.swift
+//  dogInstagram
+//
+//  Created by 笹倉 一也 on 2017/10/31.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
--- dogInstagram/ViewController.swift
+++ dogInstagram/ViewController.swift
@@ -7,45 +7,26 @@
 //
 
 import UIKit
+import Social
 
 
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
- 
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     
-
-
-
-    
-    
-    
-    
-
-
-    
-
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
- 
-    
-    
-    
-    
-    
-    
     
 }
 
