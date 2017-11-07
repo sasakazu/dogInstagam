383a4550e86c4c2f7a8ade428020540faa4a1c9d
sasakazu <dropout.college2014@gmail.com>
2017-11-07 18:46:57 +0900

    uiimagepickercontroller done....

--- dogInstagram/ViewController2.swift
+++ dogInstagram/ViewController2.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var cameraView: UIImageView!
     
     var textVC2:String?
     
@@ -17,10 +18,113 @@ class ViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = textVC2
+         label.text = "カメラボタンを押すとカメラ起動！"
+    
     }
 
 
+    @IBAction func startCamera(_ sender: Any) {
+        
+        let sourceType:UIImagePickerControllerSourceType =
+            UIImagePickerControllerSourceType.camera
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            label.text = "error"
+            
+        }
+        
+        
+    }
+    
+    
+    
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage]
+            as? UIImage {
+            
+            cameraView.contentMode = .scaleAspectFit
+            cameraView.image = pickedImage
+            
+        }
+        
+        //閉じる処理
+        imagePicker.dismiss(animated: true, completion: nil)
+        label.text = "Tap the [Save] to save a picture"
+        
+    }
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        label.text = "Canceled"
+    }
+    
+    
+    
+    @IBAction func savePictute(_ sender: Any) {
+        
+        let image:UIImage! = cameraView.image
+        
+        if image != nil {
+            UIImageWriteToSavedPhotosAlbum(image, self,
+                                           #selector(ViewController2.image(_:didFinishSavingWithError:contextInfo:)),
+                                           nil)
+        }
+        else{
+            label.text = "image Failed !"
+        }
+    }
+    
+    
+    // 書き込み完了結果の受け取り
+    @objc func image(_ image: UIImage,
+                     didFinishSavingWithError error: NSError!,
+                     contextInfo: UnsafeMutableRawPointer) {
+        
+        if error != nil {
+            print(error.code)
+            label.text = "Save Failed !"
+        }
+        else{
+            label.text = "Save Succeeded"
+        }
+    }
+    
+    @IBAction func showAlbum(_ sender: Any) {
+        
+        let sourceType:UIImagePickerControllerSourceType =
+            UIImagePickerControllerSourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+            label.text = "Tap the [Start] to save a picture"
+        }
+        else{
+            label.text = "error"
+            
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
